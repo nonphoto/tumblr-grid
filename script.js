6a8919413626b23af5622ffe9ff6a7d3ec2d39c5
Jonas Luebbers <jluebbers@squarespace.com>
2017-03-27 21:20:45 -0400

    Make api request using axios

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var apiKey = "XriRAsdFawgr7IsOMsK7QARfi4kY3zD1myqBL10rqW9JZmjJO8";
-var getAmount = 50;
+var apiKey = 'XriRAsdFawgr7IsOMsK7QARfi4kY3zD1myqBL10rqW9JZmjJO8'
+var limit = 50
+var imageCount = 0
 
 // type: "GET",
 // url: "https://api.tumblr.com/v2/blog/" + host_name + ".tumblr.com/likes?api_key=" + api_key,
@@ -8,3 +9,23 @@ var getAmount = 50;
 // 	"limit": get_amount,
 // 	"offset": photo_count
 // },
+
+function loadImages(username) {
+	axios.get(`https://api.tumblr.com/v2/blog/${username}.tumblr.com/likes`, {
+		params: {
+			api_key: apiKey
+		},
+		limit: limit,
+		offset: imageCount
+	})
+		.then((response) => {
+			console.log(response)
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	loadImages('jonasluebbers')
+})
