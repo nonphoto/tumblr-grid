d745f19b773aa94ec7673170c7fbe1c89f983f7a
Jonas Luebbers <jluebbers@squarespace.com>
2017-04-15 01:30:56 -0400

    Refactor element construction into separate function

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
+const apiKey = 'XriRAsdFawgr7IsOMsK7QARfi4kY3zD1myqBL10rqW9JZmjJO8'
 const scrollThreshold = 100
 const postLimit = 50
-
-const params = {
-	api_key: 'XriRAsdFawgr7IsOMsK7QARfi4kY3zD1myqBL10rqW9JZmjJO8',
-	limit: postLimit
-}
 
 let username = null
 let postCount = 0
 let lastPostTimestamp = 0
 let isLoading = false
 let hasPostsRemaining = true
-
-let hint = null
-let spinner = null
-let form = null
-let formText = null
-let viewer = null
-let viewerImage = null
-let tileContainer = null
 
 function request(url, params) {
 	params.callback = 'requestCallback'
@@ -50,87 +38,42 @@ function request(url, params) {
 	return promise
 }
 
-function loadImages() {
-	if (!username) return
-	if (isLoading) return
-	if (!hasPostsRemaining) return
+function createFragmentForPosts(posts) {
+	const fragment = document.createDocumentFragment()
 
-	isLoading = true
+	posts.forEach(post => {
+		if (post.type === 'photo') {
+			post.photos.forEach(photo => {
+				const tile = document.createElement('div')
+				tile.classList.add('tile')
 
-	spinner.classList.add('is-visible')
+				const fullImage = photo.alt_sizes[0]
+				const thumbnailImage = photo.alt_sizes[photo.alt_sizes.length - 1]
 
-	const onLoadEnd = () => {
-		spinner.classList.remove('is-visible')
-		isLoading = false
-	}
+				tile.style.backgroundImage = `url(${thumbnailImage.url})`
+				tile.addEventListener('click', () => {
+					viewer.classList.add('is-active')
+					viewerImage.src = fullImage.url
+					viewerImage.dataset.href = post.post_url
+				})
 
-	if (postCount <= 1000) {
-		params.offset = postCount
-		params.before = undefined
-	}
-	else {
-		params.offset = undefined
-		params.before = lastPostTimestamp
-	}
+				fragment.appendChild(tile)
+			})
+		}
+		else {
+			const tile = document.createElement('div')
+			tile.classList.add('tile')
 
-	request(`https://api.tumblr.com/v2/blog/${username}.tumblr.com/likes`, params)
-		.then((data) => {
-
-			const fragment = document.createDocumentFragment()
-			const posts = data.response.liked_posts
-
-			if (posts.length <= 0) {
-				hasPostsRemaining = false
-				onLoadEnd()
-				return
-			}
-
-			posts.forEach(post => {
-				if (post.type === 'photo') {
-					post.photos.forEach(photo => {
-						const tile = document.createElement('div')
-						tile.classList.add('tile')
-
-						const fullImage = photo.alt_sizes[0]
-						const thumbnailImage = photo.alt_sizes[photo.alt_sizes.length - 1]
-
-						tile.style.backgroundImage = `url(${thumbnailImage.url})`
-						tile.addEventListener('click', () => {
-							viewer.classList.add('is-active')
-							viewerImage.src = fullImage.url
-							viewerImage.dataset.href = post.post_url
-						})
-
-						fragment.appendChild(tile)
-					})
-				}
-				else {
-					const tile = document.createElement('div')
-					tile.classList.add('tile')
-
-					tile.classList.add('has-no-image')
-					tile.addEventListener('click', () => {
-						window.open(post.post_url, '_blank')
-					})
-
-					fragment.appendChild(tile)
-				}
+			tile.classList.add('has-no-image')
+			tile.addEventListener('click', () => {
+				window.open(post.post_url, '_blank')
 			})
 
-			lastPostTimestamp = posts[posts.length - 1].timestamp
-			postCount += postLimit
-			tileContainer.appendChild(fragment)
+			fragment.appendChild(tile)
+		}
+	})
 
-			onLoadEnd()
-
-			if (hasScrolledToBottom()) {
-				loadImages()
-			}
-		})
-		.catch((error) => {
-			console.error(error)
-			onLoadEnd()
-		})
+	return fragment
 }
 
 function hasScrolledToBottom() {
@@ -138,13 +81,13 @@ function hasScrolledToBottom() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	hint = document.getElementById('hint')
-	spinner = document.getElementById('spinner')
-	form = document.getElementById('form-username')
-	formText = document.getElementById('text-username')
-	viewer = document.getElementById('viewer')
-	viewerImage = document.getElementById('viewer-image')
-	tileContainer = document.getElementById('tile-container')
+	const hint = document.getElementById('hint')
+	const spinner = document.getElementById('spinner')
+	const form = document.getElementById('form-username')
+	const formText = document.getElementById('text-username')
+	const viewer = document.getElementById('viewer')
+	const viewerImage = document.getElementById('viewer-image')
+	const tileContainer = document.getElementById('tile-container')
 
 	form.addEventListener('submit', () => {
 		hint.classList.add('is-hidden')
@@ -175,4 +118,58 @@ document.addEventListener('DOMContentLoaded', () => {
 			loadImages()
 		}
 	})
+
+	const loadImages = () => {
+		if (!username) return
+		if (isLoading) return
+		if (!hasPostsRemaining) return
+
+		isLoading = true
+
+		spinner.classList.add('is-visible')
+
+		const onLoadEnd = () => {
+			spinner.classList.remove('is-visible')
+			isLoading = false
+		}
+
+		const params = {
+			api_key: 'XriRAsdFawgr7IsOMsK7QARfi4kY3zD1myqBL10rqW9JZmjJO8',
+			limit: postLimit
+		}
+
+		if (postCount < 1000) {
+			params.offset = postCount
+		}
+		else {
+			params.before = lastPostTimestamp
+		}
+
+		request(`https://api.tumblr.com/v2/blog/${username}.tumblr.com/likes`, params)
+			.then((data) => {
+				const posts = data.response.liked_posts
+
+				if (posts.length <= 0) {
+					hasPostsRemaining = false
+					onLoadEnd()
+					return
+				}
+
+				const fragment = createFragmentForPosts(posts)
+				tileContainer.appendChild(fragment)
+
+				lastPostTimestamp = posts[posts.length - 1].timestamp
+				postCount += postLimit
+
+				onLoadEnd()
+
+				if (hasScrolledToBottom()) {
+					loadImages()
+				}
+			})
+			.catch((error) => {
+				console.error(error)
+				onLoadEnd()
+			})
+	}
 })
